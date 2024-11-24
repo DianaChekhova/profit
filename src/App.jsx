692828dfe3b2175508afe74090ea7efb131a7887
skeez <skeezmo_new@mail.ru>
2024-11-24 12:52:00 +0300

    update dependancy, router + ui kit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(12);
 
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-
-  useEffect(() => {
-    console.log('popa');
-  }, []);
   return (
     <>
       <button onClick={() => setCount(count)}></button>
