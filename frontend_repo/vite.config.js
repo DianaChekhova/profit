144b51ec32679b73566edc3739f40639783133ca
skeez <skeezmo_new@mail.ru>
2025-01-03 21:18:12 +0300

    fix port VITE

--- frontend_repo/vite.config.js
+++ frontend_repo/vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+      },
+    },
   },
   plugins: [react()],
 });
