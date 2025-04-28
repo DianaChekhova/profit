20bfbc7ca784428ada11c7de01e9e2edce336bc0
Diana <chexov8465@gmail.com>
2025-04-28 22:06:44 +0300

    EGOR

--- backend_repo/models/trainer.go
+++ backend_repo/models/trainer.go
@@ -1,10 +1,10 @@
 package models
 
 type Trainer struct {
-	ID        string
-	Name      string
-	Email     string
-	Specialty string
-	Password  string
-	Schedule  []Schedule
+	ID        string     `json:"id,omitempty" bson:"id"`
+	Name      string     `json:"name,omitempty" bson:"name"`
+	Email     string     `json:"email,omitempty" bson:"email"`
+	Specialty string     `json:"specialty,omitempty" bson:"specialty"`
+	Password  string     `json:"password,omitempty" bson:"password"`
+	Schedule  []Schedule `json:"schedule,omitempty" bson:"schedule"`
 }
