75bc9c3302d6dffc3720a93e247ce108e1eb01da
Diana <chexov8465@gmail.com>
2025-04-29 23:07:36 +0300

    asdasd

--- backend_repo/models/trainer.go
+++ backend_repo/models/trainer.go
@@ -1,7 +1,7 @@
 package models
 
 type Trainer struct {
-	ID        string     `json:"id" bson:"_id"`
+	ID        string     `json:"id" bson:"_id,omitempty"`
 	Name      string     `json:"name" bson:"name"`
 	Email     string     `json:"email" bson:"email"`
 	Specialty string     `json:"specialty" bson:"specialty"`
