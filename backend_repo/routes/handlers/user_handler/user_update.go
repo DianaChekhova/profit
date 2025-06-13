4cc4c04e1e062cc19434117d229957a6c20c2a0e
Diana <chexov8465@gmail.com>
2025-06-13 18:01:13 +0300

    ASDASD

--- backend_repo/routes/handlers/user_handler/user_update.go
+++ backend_repo/routes/handlers/user_handler/user_update.go
@@ -4,19 +4,24 @@ import (
 	"encoding/json"
 	"net/http"
 	"profit/models"
+	"profit/routes/auth/protection"
 	"profit/routes/handlers/backendController"
 )
 
 func (uc *UserController) UserUpdate(w http.ResponseWriter, r *http.Request) {
 	var user models.User
-
+	userID := r.Context().Value(protection.ContextUserIDKey).(string)
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		http.Error(w, "Database error", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
+	user.ID = userID
+
 	if err := uc.userRepo.UpdateUser(r.Context(), &user); err != nil {
-		http.Error(w, "Database error", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	backendController.WriteJSONResponse(
