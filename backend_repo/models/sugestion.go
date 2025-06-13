3bbcc1c1a57bfa3dbcd950f76a4a04e80f09f106
Diana <chexov8465@gmail.com>
2025-06-13 20:50:24 +0300

    asd

--- backend_repo/models/sugestion.go
+++ backend_repo/models/sugestion.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Suggestion struct {
-	Oid      primitive.ObjectID `json:"id,omitempty" bson:"id,omitempty"`
+	Oid      primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	Name     string             `json:"name" bson:"name"`
 	Email    string             `json:"email" bson:"email"`
 	Number   string             `json:"number" bson:"number"`
