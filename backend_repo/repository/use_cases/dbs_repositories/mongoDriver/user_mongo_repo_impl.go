65bb66f80273457103ba786e110b1d22e7a0cf9e
Diana <chexov8465@gmail.com>
2025-06-13 19:29:12 +0300

    asdasd

--- backend_repo/repository/use_cases/dbs_repositories/mongoDriver/user_mongo_repo_impl.go
+++ backend_repo/repository/use_cases/dbs_repositories/mongoDriver/user_mongo_repo_impl.go
@@ -2,6 +2,7 @@ package mongoDriver
 
 import (
 	"context"
+	"errors"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -62,15 +63,46 @@ func (repo *UserMongoRepository) UsersList(ctx context.Context) ([]*models.User,
 	return users, cursor.Err()
 }
 
+func convertDtoUserToMgdb(u *models.User) *models.User {
+	return &models.User{
+		ID:       u.ID,
+		Email:    u.Email,
+		Password: u.Password,
+	}
+}
 func (repo *UserMongoRepository) UpdateUser(ctx context.Context, user *models.User) error {
 	oid, err := primitive.ObjectIDFromHex(user.ID)
 	if err != nil {
 		return err
 	}
+
 	filter := bson.M{"_id": oid}
-	update := bson.M{"$set": user}
-	_, err = repo.collection.UpdateOne(ctx, filter, update)
-	return err
+
+	updateData := bson.M{
+		"phone_number": user.PhoneNumber,
+		"name":         user.UserName,
+		"email":        user.Email,
+		"password":     user.Password,
+		"created_at":   user.CreatedAt,
+		"role":         user.Role,
+		"schedule":     user.ScheduleIds,
+		"subscription": user.Subscription,
+		"address":      user.Address,
+		"birth":        user.BirthDate,
+		"passport":     user.Passport,
+		"status":       user.Status,
+	}
+
+	update := bson.M{"$set": updateData}
+	res, err := repo.collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return err
+	}
+
+	if res.MatchedCount == 0 {
+		return errors.New("document not found for update, filter:")
+	}
+	return nil
 }
 
 func (repo *UserMongoRepository) CreateUser(ctx context.Context, user *models.User) error {
