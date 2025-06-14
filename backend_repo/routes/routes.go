9ab5ecfb46e4443d0910c71dec7565e2f8334cb2
Diana <chexov8465@gmail.com>
2025-06-14 16:49:39 +0300

    ASD

--- backend_repo/routes/routes.go
+++ backend_repo/routes/routes.go
@@ -88,7 +88,7 @@ func InitRoutes(db *mongo.Database, ctx context.Context) http.Handler {
 
 		// User routes
 		r.Route("/users", func(r chi.Router) {
-			r.Use(middleware.AdminMiddleware)
+			r.Use(middleware.UserMiddleware)
 			r.Get("/", userController.UserList)
 
 			r.Get("/{id}", userController.GetUser)
