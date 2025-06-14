24b4b7b213a6bacdf85e58f16932f586bcfd62cf
Diana <chexov8465@gmail.com>
2025-06-14 17:06:43 +0300

    ad

--- backend_repo/routes/routes.go
+++ backend_repo/routes/routes.go
@@ -94,7 +94,7 @@ func InitRoutes(db *mongo.Database, ctx context.Context) http.Handler {
 			r.Delete("/{id}", userController.DeleteUser)
 
 			r.Put("/{id}/status", userController.UpdateUserStatus)
-			r.Put("/{id}", userController.AddUser)
+			r.Put("/{id}", userController.UserUpdate)
 		})
 
 		r.Route("/admin", func(r chi.Router) {
@@ -134,7 +134,6 @@ func InitRoutes(db *mongo.Database, ctx context.Context) http.Handler {
 			})
 
 			r.Route("/personal", func(r chi.Router) {
-				r.Use(middleware.TrainerMiddleware)
 				r.Get("/schedule", subscriptionController.GetPersonalSchedule)
 				r.Post("/schedule/{id}/register", subscriptionController.RegisterForPersonalSession)
 				r.Delete("/schedule/{id}/register", subscriptionController.UnregisterFromPersonalSession)
